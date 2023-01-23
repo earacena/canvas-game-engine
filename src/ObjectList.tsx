1afdf50929cb008ff93cc367ae18bb688da73b06
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-01-22 23:11:12 -0500

    Fix structure of ObjectList list items

--- src/ObjectList.tsx
+++ src/ObjectList.tsx
@@ -92,7 +92,7 @@ function ObjectList({
   const objectPropertiesStyle = 'text-sm text-slate-500 pr-1';
 
   return (
-    <div className="w-96">
+    <div className="w-fit">
       Objects
       {' '}
       <button
@@ -116,71 +116,75 @@ function ObjectList({
       <ObjectForm objectFormOpened={objectFormOpened} addBlock={addBlock} />
       <ul className={`bg-slate-400 rounded-md p-1 ${blocks.length === 0 ? 'hidden' : ''} `}>
         {blocks.map((b) => (
-          <button
-            key={b.id}
-            type="button"
-            className={`flex flex-row items-center bg-slate-100 p-4 w-auto rounded-md m-2 ${
-              selectedTargetId === b.id ? 'border-gray-900 border-4' : ''
-            }`}
-            onClick={() => setSelectedTargetId(b.id)}
-          >
-            <BsSquareFill className="pr-5" color={b.color} size={50} />
-            <div className="flex flex-col items-start">
-              <button type="button" onClick={() => handleDelete(b.id)}>
-                <BsTrash />
-              </button>
-              <div>
-                <span className={objectPropertiesStyle}>NAME</span>
-                {b.name}
-              </div>
-              <div>
-                <span className={objectPropertiesStyle}>X</span>
-                {b.x}
-              </div>
-              <div>
-                <span className={objectPropertiesStyle}>Y</span>
-                {b.y}
-              </div>
-              <div>
-                <span className={objectPropertiesStyle}>WIDTH</span>
-                {b.w}
-              </div>
-              <div>
-                <span className={objectPropertiesStyle}>HEIGHT</span>
-                {b.h}
-              </div>
+          <div className="flex flex-row">
+            <button type="button" onClick={() => handleDelete(b.id)}>
+              <BsTrash />
+            </button>
+            <button
+              key={b.id}
+              type="button"
+              className={`flex flex-row items-center bg-slate-100 p-4 rounded-md m-2 ${
+                selectedTargetId === b.id ? 'border-gray-900 border-4' : ''
+              }`}
+              onClick={() => setSelectedTargetId(b.id)}
+            >
+              <BsSquareFill className="pr-5" color={b.color} size={50} />
               <div className="flex flex-col items-start">
-                <span className={objectPropertiesStyle}>TEXTURE</span>
-                <input
-                  type="file"
-                  onChange={(event: ChangeEvent<HTMLInputElement>) => handleFileUpload(b.id, event)}
-                  accept=".jpg,.jpeg,.png"
-                />
+                <div>
+                  <span className={objectPropertiesStyle}>NAME</span>
+                  {b.name}
+                </div>
+                <div>
+                  <span className={objectPropertiesStyle}>X</span>
+                  {b.x}
+                </div>
+                <div>
+                  <span className={objectPropertiesStyle}>Y</span>
+                  {b.y}
+                </div>
+                <div>
+                  <span className={objectPropertiesStyle}>WIDTH</span>
+                  {b.w}
+                </div>
+                <div>
+                  <span className={objectPropertiesStyle}>HEIGHT</span>
+                  {b.h}
+                </div>
+                <div className="flex flex-col items-start">
+                  <span className={objectPropertiesStyle}>TEXTURE</span>
+                  <input
+                    type="file"
+                    onChange={
+                      (event: ChangeEvent<HTMLInputElement>) => handleFileUpload(b.id, event)
+                    }
+                    accept=".jpg,.jpeg,.png"
+                  />
+                </div>
+                <label
+                  id="controllable-checkbox-label"
+                  htmlFor="controllable-checkbox"
+                >
+                  Controllable (WASD)?
+                  <input
+                    id="controllable-checkbox"
+                    type="checkbox"
+                    onChange={(event) => handleSetControllable(event, b.id)}
+                  />
+                </label>
+                <label
+                  id="locked-camera-checkbox-label"
+                  htmlFor="locaked-camera-checkbox"
+                >
+                  Lock camera?
+                  <input
+                    id="locked-camera-checkbox"
+                    type="checkbox"
+                    onChange={(event) => handleSetLocked(event, b.id)}
+                  />
+                </label>
               </div>
-              <label
-                id="controllable-checkbox-label"
-                htmlFor="controllable-checkbox"
-              >
-                Controllable (WASD)?
-                <input
-                  id="controllable-checkbox"
-                  type="checkbox"
-                  onChange={(event) => handleSetControllable(event, b.id)}
-                />
-              </label>
-              <label
-                id="locked-camera-checkbox-label"
-                htmlFor="locaked-camera-checkbox"
-              >
-                Lock camera?
-                <input
-                  id="locked-camera-checkbox"
-                  type="checkbox"
-                  onChange={(event) => handleSetLocked(event, b.id)}
-                />
-              </label>
-            </div>
-          </button>
+            </button>
+          </div>
         ))}
       </ul>
     </div>
